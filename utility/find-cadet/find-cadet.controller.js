d625e3ec41ce1a596b4a49766b4111a48196a93e
JackEveleigh <47282333+JackEveleigh@users.noreply.github.com>
2019-03-06 13:35:22 -0500

    Updated find-cadet.controller

--- utility/find-cadet/find-cadet.controller.js
+++ utility/find-cadet/find-cadet.controller.js
@@ -3,79 +3,42 @@
 
 angular.module('findApp').
     controller( "FindCadetController", function ($scope, $http, $window) {
+                $http({
+                    method: 'POST',
+                    url: '../../php/findCadets.php',
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                }).then(function (response) {
+                    $scope.cadets = response.data.data;
+                });
 
-        // List of cadets picked on the Find Cadet View. This is instead of a
-        // dictionary so that an order on when things are picked can be
-        // maintained.
-        $scope.pickedCadets = [];
-
-        // Keeps track of which of the cadets have been checked
-        $scope.checkedCadets = {};
-
-
-        $http({
-            method: 'POST',
-            url: '../../php/findCadets.php',
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-        }).then(function (response) {
-            $scope.cadets = response.data.data;
-        });
+                $scope.pickCadet = function (cadet) {
+                    //Storing fkCadetID to be used by another view
+                    //CLEAN UP -- only needs to store CadetID to localStorage
+                    $window.sessionStorage.setItem("fkCadetID", cadet.fkCadetID);
+                    $window.localStorage.setItem("fkCadetID", cadet.fkCadetID);
+                    $window.sessionStorage.setItem("CadetID", cadet.fkCadetID);
+                    $window.sessionStorage.setItem("theCadet", cadet.fkCadetID);
+                    $window.localStorage.setItem("theCadet", cadet.fkCadetID);
 
 
-        /**
-         * Serves as a toggle for the checkboxes on the Find Cadet View. When
-         * the checkbox for a cadet is checked, the respective cadet is added to
-         * the list of `pickedCadets`. When the checkbox is unchecked, the cadet
-         * is removed.
-         *
-         * - Parameters:
-         *   - cadet: The cadet being either added or removed from the list of
-         *   `pickedCadets`.
-         */
-        $scope.selectCadet = function(cadet) {
-            if(!$scope.checkedCadets[cadet.fkCadetID]) {
-                var index = $scope.pickedCadets.indexOf(cadet);
-                if(index >= 0) {
-                    $scope.pickedCadets.splice(index, 1);
+
+                    $window.localStorage.setItem("CadetID", cadet.fkCadetID);
+                    $window.localStorage.setItem("CadetName", cadet.PersonFN + " " + cadet.PersonLN);
+                    //EDIT set items were added for PGender and PDOB in order to set the gender and the dob of the cadet
+                    $window.localStorage.setItem("CadetGender",cadet.PGender);
+                    $window.localStorage.setItem("CadetDOB",cadet.PDOB);
+
+                    $scope.myCadet = {fkCadetID: cadet.fkCadetID};
+                    $http({
+                        method: 'POST',
+                        url: '../../php/getCadetViewData.php',
+                        data: Object.toparams($scope.myCadet),
+                        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                    }).then(function (response) {
+                            var cadet = response.data.data;
+
+                            $scope.updateDisplay = "./cadet-helper.view.html" + "?updated=" + Date.now();
+                        }
+                    );
                 }
-            } else {
-                $scope.pickedCadets.push(cadet);
-            }
-        };
-
-        $scope.saveAndClose = function() {
-            var cadetJSON = JSON.stringify($scope.pickedCadets);
-            $window.sessionStorage.setItem("cadets", cadetJSON);
-            $window.localStorage.setItem("cadets", cadetJSON);
-            $window.opener.location.reload();
-            window.close();
-        };
-
-        $scope.pickCadet = function (cadet) {
-            //Storing fkCadetID to be used by another view
-            //CLEAN UP -- only needs to store CadetID to localStorage
-            $window.sessionStorage.setItem("fkCadetID", cadet.fkCadetID);
-            $window.localStorage.setItem("fkCadetID", cadet.fkCadetID);
-            $window.sessionStorage.setItem("CadetID", cadet.fkCadetID);
-            $window.sessionStorage.setItem("theCadet", cadet.fkCadetID);
-            $window.localStorage.setItem("theCadet", cadet.fkCadetID);
-
-
-
-            $window.localStorage.setItem("CadetID", cadet.fkCadetID);
-            $window.localStorage.setItem("CadetName", cadet.PersonFN + " " + cadet.PersonLN);
-
-            $scope.myCadet = {fkCadetID: cadet.fkCadetID};
-            $http({
-                method: 'POST',
-                url: '../../php/getCadetViewData.php',
-                data: Object.toparams($scope.myCadet),
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-            }).then(function (response) {
-                var cadet = response.data.data;
-
-                $scope.updateDisplay = "./cadet-helper.view.html" + "?updated=" + Date.now();
-            }
-            );
-        }
-    });
+            });
