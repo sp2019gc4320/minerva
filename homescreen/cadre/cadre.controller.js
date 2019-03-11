db63f1909e4fbb9cc933087727025dda26d94cfc
duncan <48213027+duncankwilliams@users.noreply.github.com>
2019-03-11 12:01:22 -0400

    Added cadre page and folder. Edited app.config and pagination
    Using caseManager controller and module for now, I was having trouble getting things to load using cadre controller and module.

---
+++ homescreen/cadre/cadre.controller.js
@@ -0,0 +1,53 @@
+//cadre.controller.js
+'use strict';
+// having to use homesreen.caseManager instead of homescreen.cadre for now due to issues 3/11
+// having to use caseManagerController instead of cadreController for now due to issues 3/11
+angular.module('homescreen.caseManager').
+controller("caseManagerController", function($scope, $http, $window){
+
+    $scope.CadetID = $window.localStorage.getItem("CadetID");
+    $scope.CadetName = $window.localStorage.getItem("CadetName");
+    //EDIT
+    $scope.CadetGender = $window.localStorage.getItem("CadetGender");
+    $scope.CadetDOB = $window.localStorage.getItem("CadetDOB");
+    //$scope.CadetDOB = $window.localStorage.getItem("CadetDOB");
+    //alert("DOB: " + $window.localStorage.getItem("CadetDOB"));
+
+    if($scope.CadetID === null) {
+        alert("You must select a cadet!");
+    }
+
+    $scope.cadet = {
+        CadetID: $scope.CadetID,
+        CadetName: $scope.CadetName,
+        //EDIT
+        CadetGender: $scope.CadetGender
+        //CadetDOB: $scope.CadetDOB
+    };
+
+    $scope.cadreViews = [
+        {view:'Mentor', url:'./case-manager/cadet-mentor/updateMentor.view.html'},
+        {view:'Citizenship', url:'./core-components/citizenship/citizenship.view.html'},
+        {view:'Health & Hygiene', url:'./core-components/health/health.view.html'},
+        {view:'Lead/Follow', url:'./core-components/lead-follow/lead-follow.view.html'},
+        {view:'S2C', url:'./core-components/s2c/s2c.view.html'},
+        {view: 'Job Skills', url:'./core-components/job-skills/job-skills.view.html'},
+        {view: 'PRAP', url: './notes/prap/prap.view.html'},
+        {view: 'Post Residential', url:'./notes/postres/postres.view.html'}];
+
+    $scope.showView= function showView(item){
+        $scope.dataurl = item.url;
+        $scope.updateDisplay = item.url+ "?updated=" + Date.now();
+    };
+
+    $scope.openFindCadetView = function()
+    {
+
+        $window.open('./utility/find-cadet/find-cadet-index.view.html', "_blank",
+            "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=300,height=300");
+
+    };
+
+
+
+});
--- homescreen/homescreen.module.js
+++ homescreen/homescreen.module.js
@@ -1,6 +1,7 @@
 //File: homescreen.module.js
 
 'use strict';
+angular.module('homescreen',['homescreen.cadre']);
 
 angular.module('homescreen', [
     'homescreen.caseManager'
