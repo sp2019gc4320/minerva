bc7f48fceb8a9dee015cf2b03399a1f6974c2592
Graham <schunkg@gmail.com>
2019-04-07 21:58:45 -0400

    Added Rough Draft of upload function
    added rough draft of update function. Does not have much detail just to see if we can get it working

--- website/edit-corecomponent/core-edit.controller.js
+++ website/edit-corecomponent/core-edit.controller.js
@@ -2,4 +2,40 @@ angular.module('website.edit-corecomponent').controller('coreTasksController', f
 	
 	$scope.editable= false;
 
+
+
+	$scope.update= function(){
+
+		for(var i=0; i<$scope.tasks.length;i++){
+
+			$scope.numSaved=0;
+			var sendData=angular.copy($scope.tasks[j]);
+			$http ({
+                method: 'POST',
+                url: "./php/updateCoreTasks.php",
+                data: Object.toparams(sendData),
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            }).then(
+                function(response)
+                {
+
+                   if(response.data) {
+
+                        //this only needs to be saved once all changes are saved.
+                        $scope.numSaved++;
+                         if($scope.numSaved == $scope.tasks.length)
+                            alert("Changes Saved.");
+                    }
+                    //location.reload(true);
+                },function(result){
+                    alert("Failed");
+                });
+        }
+        $scope.editable = false;
+    };
+		
+
+
+	
+
 }
