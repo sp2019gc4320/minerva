fb70352956b2f7d9e01f0c44fdfa71a38bdf8a2f
laurenalibey7 <47374935+laurenalibey7@users.noreply.github.com>
2019-04-08 09:22:13 -0400

    Add files via upload

---
+++ php/site-viewcadets.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lauren Alibey
+ * Date: 4/3/2019
+ * Time: 7:05 PM
+ */
+
+require_once 'dbcontroller.php';
+
+//create connection
+$conn = new DBController();
+//$res = array();
+
+
+//Create Field List
+$fields = "PersonFN, PersonLN, fkClassDetailID, CadetRosterNumber, fkCadetID";
+
+$sql = "SELECT tblPeople.PersonID, tblPeople.PersonFN, tblPeople.PersonLN, tblClassDetails.fkCadetID, tblClassDetails.ClassDetailID, tblClassDetails.fkClassID, tblClassDetails.CadetRosterNumber;
+       FROM tblMentorPotential RIGHT JOIN (tblPeople INNER JOIN (tblCadets INNER JOIN tblClassDetails ON tblCadets.CadetID = tblClassDetails.fkCadetID) ON tblPeople.PersonID = tblCadets.fkPersonID) ON tblMentorPotential.fkClassDetailID = tblClassDetails.ClassDetailID";
+
+$result = $conn->runSelectQuery($sql);
+echo '{ "cadetTable":[';
+if($result->num_rows >0)
+{
+    $count = 0;
+
+    while($row = $result-> fetch_assoc()){
+
+        if($count >0)
+            echo ",";
+
+        echo'{"PersonFN": "'.$row["PersonFN"]. '" , "PersonLN": "'.$row["PersonLN"]. '" , "fkClassID": "'.$row["fkClassDetailID"].
+            '" , "CadetRosterNumber": "'.$row["CadetRosterNumber"]. '" , "fkCadetID": "'.$row["fkCadetID"].'"}';
+        $count = $count+1;
+    }
+
+}
+echo']}';
+/*if($result-> num_rows > 0){
+    while($row = $result->fetch_assoc()){
+
+        $res = array_merge($res, [$row]);
+    }
+    echo(json_encode($res));
+}
+else{
+    echo("could not retrieve cadets");
+}*/
+?>
