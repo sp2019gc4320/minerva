4e90de39ba12ff9622dc95eb9f59c2c0d4d4d109
laurenalibey7 <47374935+laurenalibey7@users.noreply.github.com>
2019-04-25 13:34:24 -0400

    Add files via upload
    Changed order of buttons--just to make it flow better.

--- admin/admin.controller.js
+++ admin/admin.controller.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 angular.module('admin').
@@ -12,8 +13,8 @@ controller('ctrl_homeOptions', function($scope, $http, $window) {
         {view:'Add Cadet', url:'./admin/site-addcadet/site-addcadet.view.html'},
         {view:'View Cadets', url:'./admin/site-viewcadets/site-viewcadets.view.html'},
         {view:'View Graduates', url:'./admin/site-viewgraduates/site-viewgraduates.view.html'},
-        {view:'Assign Company Staff', url:'./admin/site-compassign/compassign.view.html'},
-        {view:'View Applicants', url:'./admin/site-viewapplicants/site-viewapplicants.view.html'}
+        {view:'View Applicants', url:'./admin/site-viewapplicants/site-viewapplicants.view.html'},
+        {view:'Assign Company Staff', url:'./admin/site-compassign/compassign.view.html'}
     ]
 
     $scope.showView = function showView(item){
