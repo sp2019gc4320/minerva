1c852ea2ad7241a5e27cc00ca74fbf74b195a8c5
TParse <47325213+TParse@users.noreply.github.com>
2019-04-25 13:02:04 -0400

    Uploading most recent version

--- recruiter/applicant-find/applicant-find.controller.js
+++ recruiter/applicant-find/applicant-find.controller.js
@@ -2,16 +2,57 @@
 
 angular.module('recruiter.applicantFind').controller('applicantFindController', function($scope, $http, $window) 
 {
-    $scope.cadets = JSON.parse($window.localStorage.getItem("cadets"));
-    $scope.CadetID = $window.localStorage.getItem("CadetID");
-    $scope.CadetName = $window.localStorage.getItem("CadetName");
-    $scope.CadetGender = $window.localStorage.getItem("CadetGender");
-    $scope.CadetDOB = $window.localStorage.getItem("CadetDOB");
-    
-    $scope.cadet = {
-        CadetID: $scope.CadetID,
-        CadetName: $scope.CadetName,
-        CadetGender: $scope.CadetGender,
-        CadetDOB: $scope.CadetDOB
+    $scope.applicants = JSON.parse($window.localStorage.getItem("applicants"));
+    $scope.ApplicantID = $window.localStorage.getItem("ApplicantID");
+    $scope.ApplicantName = $window.localStorage.getItem("ApplicantName");
+    $scope.ApplicantGender = $window.localStorage.getItem("ApplicantGender");
+    $scope.ApplicantDOB = $window.localStorage.getItem("ApplicantDOB");
+
+    $scope.applicant = {
+        ApplicantID: $scope.ApplicantID,
+        ApplicantName: $scope.ApplicantName,
+        ApplicantGender: $scope.ApplicantGender,
+        ApplicantDOB: $scope.ApplicantDOB
     };
+
+    
+    var taskListFile = $http({
+        method: 'POST',
+        url: './php/app_fileList.php',
+        data: '',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+
+    });
+    taskListFile.then(
+        //Will use this statement once we have applicant ID functioning fully
+        //var applicantID = $scope.applicantID
+            function(result){
+                alert("Success");
+
+                $scope.fileList = result.data.data;
+
+                alert(JSON.stringify($scope.fileList));
+
+                
+                $scope.missingList = ["EducationPlan","BandARecods","MedicalInsurance",
+                 "Immunization","CandidateApplication",
+            "MedicalHistory","BirthCertificate","LegalHistory","MentorApplication",
+            "SocialSecurityCard","IDCard","MentalHealthHistory"];
+
+            for (var i = 0; i < $scope.fileList.length; i++){
+                if($scope.missingList.includes(String($scope.fileList[i]["File"]))){
+                    var index = $scope.missingList.indexOf(String($scope.fileList[i]["File"]));
+                    $scope.missingList.splice(index,1);
+                }
+            }
+            alert(JSON.stringify($scope.missingList));
+            },
+            function(result){
+                alert("Failure");
+            }
+);
+
+    $(document).ready(function() {
+        $('#example').DataTable();
+      });
 });
--- recruiter/recruiter.controller.js
+++ recruiter/recruiter.controller.js
@@ -5,19 +5,14 @@ angular.module('recruiter').controller('recController', function($scope, $http, 
    // $window.localStorage.setItem("lookupTable",null);
     // Model for containing the Person to be added to tblPerson
     $scope.person = {};
-    $scope.applicant = {};
 
-    $scope.cadets = JSON.parse($window.localStorage.getItem("cadets"));
-    $scope.CadetID = $window.localStorage.getItem("CadetID");
-    $scope.CadetName = $window.localStorage.getItem("CadetName");
-    $scope.CadetGender = $window.localStorage.getItem("CadetGender");
-    $scope.CadetDOB = $window.localStorage.getItem("CadetDOB");
+    $scope.applicants = JSON.parse($window.localStorage.getItem("applicants"));
+    $scope.ApplicantID = $window.localStorage.getItem("ApplicantID");
+    $scope.ApplicantName = $window.localStorage.getItem("ApplicantName");
     
-    $scope.cadet = {
-        CadetID: $scope.CadetID,
-        CadetName: $scope.CadetName,
-        CadetGender: $scope.CadetGender,
-        CadetDOB: $scope.CadetDOB
+    $scope.applicant = {
+        ApplicantID: $scope.ApplicantID,
+        ApplicantName: $scope.ApplicantName
     };
 
     // Mailing address toggle
@@ -78,7 +73,7 @@ angular.module('recruiter').controller('recController', function($scope, $http, 
     // Set up options on load
     $http({
         method: "GET",
-        url: "./php/admin_createCadetFormOptions.php",
+        url: "./php/admin_createApplicantFormOptions.php",
         headers: { 'Content-Type': 'application/x-www-form-urlencoded'},
     }).then(function(response) {
         var data = response.data;
@@ -118,7 +113,7 @@ angular.module('recruiter').controller('recController', function($scope, $http, 
 
     /**
      * Calculates the age given a person's DOB in MM/DD/YYYY format. Used in the
-     * view to tell the person what age a cadet is.
+     * view to tell the person what age a Applicant is.
      *
      * - Parameters:
      *   - dateString: The string containing the person's birth date
@@ -154,7 +149,7 @@ angular.module('recruiter').controller('recController', function($scope, $http, 
     }
 
     /**
-     * Submits the form data to php/admin_createCadet.php
+     * Submits the form data to php/admin_createApplicant.php
      */
     $scope.submit = function() {
         $http({
@@ -191,13 +186,13 @@ angular.module('recruiter').controller('recController', function($scope, $http, 
     //Mock Data used for testing ----------------------
     $scope.fileData = {
         Category: "Category",
-        CadetID:"208",
+        ApplicantID:"208",
         File: "testB.html",
         Description:"Description of File"
     };
     $scope.fileData2 = {
         Category: "Category2",
-        CadetID:"208",
+        ApplicantID:"208",
         File: "File2",
         Description:"Description of File Two"
     };
@@ -207,12 +202,12 @@ angular.module('recruiter').controller('recController', function($scope, $http, 
     $scope.files = [];
     $scope.files[0] = $scope.file;
     $scope.files[1] = angular.copy($scope.fileData2);
-    //Set Cadet ID to value stored in local storage
+    //Set Applicant ID to value stored in local storage
 
-    //TODO: Once this is changed to applicantID, send to backend in place of cadet
-    $scope.cadetID = JSON.parse($window.localStorage.getItem("CadetID"));
+    //TODO: Once this is changed to applicantID, send to backend in place of Applicant
+    $scope.applicantID = JSON.parse($window.localStorage.getItem("ApplicantID"));
 
-    //Upload File - Specify CadetID, PATH and File
+    //Upload File - Specify ApplicantID, PATH and File
     $scope.uploadFile =  function(docType) {
         var currDirectory='mentorFiles';
 
@@ -221,7 +216,7 @@ angular.module('recruiter').controller('recController', function($scope, $http, 
 
 
         var formData = new FormData();
-        formData.append('CadetID',$scope.cadetID);
+        formData.append('ApplicantID',$scope.applicantID);
         formData.append('directory',currDirectory);
         formData.append('fileType',docType);
         formData.append('file', myFile.files[0]);
@@ -275,7 +270,7 @@ angular.module('recruiter').controller('recController', function($scope, $http, 
 
                     var option = {
                         Category: "Category",
-                        CadetID: "208",
+                        ApplicantID: "208",
                         File: "testB.html",
                         DateAdded: "2017-04-27",
                         Description: "Description of File"
@@ -366,23 +361,91 @@ angular.module('recruiter').controller('recController', function($scope, $http, 
         //var applicantID = $scope.applicantID
             function(result){
                 //alert("Success");
+
                 $scope.fileList = result.data.data;
-                    console.log(JSON.stringify($scope.fileList));
-                    //alert(JSON.stringify($scope.fileList));
+
+                $scope.missingList = ["EducationPlan","BandARecods","MedicalInsurance",
+                 "Immunization","CandidateApplication",
+            "MedicalHistory","BirthCertificate","LegalHistory","MentorApplication",
+            "SocialSecurityCard","IDCard","MentalHealthHi0story"];
+
+            for (var i = 0; i < $scope.fileList.length; i++){
+                if($scope.missingList.includes(String($scope.fileList[i]["File"]))){
+                    var index = $scope.missingList.indexOf(String($scope.fileList[i]["File"]));
+                    $scope.missingList.splice(index,1);
+                }
+            }
             },
             function(result){
                 alert("Failure");
             }
-
-
 );
 
 $scope.openFindApplicantView = function()
 {
-    $window.open('./utility/find-cadet/find-applicant-index.view.html', "_blank",
+    $window.open('./utility/find-applicant/find-applicant-index.view.html', "_blank",
         "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=300,height=300");
 
 };
     
     $scope.showDirectory();
+
+    //To be in its own folder once controller is registered as applicantFindContoller
+    $scope.applicants = JSON.parse($window.localStorage.getItem("applicants"));
+    $scope.ApplicantID = $window.localStorage.getItem("ApplicantID");
+    $scope.ApplicantName = $window.localStorage.getItem("ApplicantName");
+    $scope.ApplicantGender = $window.localStorage.getItem("ApplicantGender");
+    $scope.ApplicantDOB = $window.localStorage.getItem("ApplicantDOB");
+
+    $scope.applicant = {
+        ApplicantID: $scope.ApplicantID,
+        ApplicantName: $scope.ApplicantName,
+        ApplicantGender: $scope.ApplicantGender,
+        ApplicantDOB: $scope.ApplicantDOB
+    };
+
+
+    var taskListFile = $http({
+        method: 'POST',
+        url: './php/app_fileList.php',
+        data: '',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+
+    });
+    taskListFile.then(
+        //Will use this statement once we have applicant ID functioning fully
+        //var applicantID = $scope.applicantID
+        function(result){
+            alert("Success");
+
+            $scope.fileList = result.data.data;
+
+            alert(JSON.stringify($scope.fileList));
+
+
+            $scope.missingList = ["EducationPlan","BandARecods","MedicalInsurance",
+                "Immunization","CandidateApplication",
+                "MedicalHistory","BirthCertificate","LegalHistory","MentorApplication",
+                "SocialSecurityCard","IDCard","MentalHealthHistory"];
+
+            for (var i = 0; i < $scope.fileList.length; i++){
+                if($scope.missingList.includes(String($scope.fileList[i]["File"]))){
+                    var index = $scope.missingList.indexOf(String($scope.fileList[i]["File"]));
+                    $scope.missingList.splice(index,1);
+                }
+            }
+            alert(JSON.stringify($scope.missingList));
+        },
+        function(result){
+            alert("Failure");
+        }
+    );
+
+    $(document).ready(function() {
+        $('#example').DataTable();
+    });
 });
+
+
+
+
