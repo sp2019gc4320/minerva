a732c2beb133249349d43983e63571e66dcbbb66
JackEveleigh <47282333+JackEveleigh@users.noreply.github.com>
2019-04-26 00:10:49 -0400

    Updated functions

--- applicant/applicant_documents.php
+++ applicant/applicant_documents.php
@@ -51,17 +51,31 @@ function listPending () {
         exit();
     }
 
-    $arr = array("Education Plan Documentation"=>"0", "School Behavioral & Attendance Records"=>"1", "Medical Insurance Forms"=>"2", "Immunization Record"=>"3", "Unoffical Academic Transcript"=>"4",
-        "Candidate Application Document Form"=>"5", "Medical History Form"=>"6", "Copy of Birth Certificate"=>"7", "Legal History Form"=>"8", "Mentor Application"=>"9", "Copy of Social Security Card"=>"10",
-        "Government Issued ID"=>"11", "Mental Health Information Form"=>"12");
-
-    /*while ($row = mysqli_fetch_array($query)) {
-        if() {
-            
-        }
-    }*/
+    $arr = array("Education Plan Documentation"=>'0', "School Behavioral & Attendance Records"=>'1', "Medical Insurance Forms"=>'2', "Immunization Record"=>'3', "Unoffical Academic Transcript"=>'4',
+        "Candidate Application Document Form"=>'5', "Medical History Form"=>'6', "Copy of Birth Certificate"=>'7', "Legal History Form"=>'8', "Mentor Application"=>'9', "Copy of Social Security Card"=>'10',
+        "Government Issued ID"=>'11', "Mental Health Information Form"=>'12');
 
     while ($row = mysqli_fetch_array($query)) {
+        foreach($arr as $x => $x_value){
+            foreach($row as $key => $value){
+                if($value==$x_value){
+
+                    //$my_string = $x;
+                    //echo $my_string . "ok";
+                    unset($arr["$x"]);
+                }
+            }
+        }
+    }
+    foreach($arr as $s => $s_value){
+        echo "<tr>";
+        echo "<td style='padding:0 60px'>".$s."</td>";
+        echo "<td><button type='button' style='width:100px;' class='btn btn-success'>Upload File</button></td>";
+        echo "<td>PDF file here</td>";
+        echo "</tr>";
+    }
+
+    /*while ($row = mysqli_fetch_array($query)) {
 
         echo "<tr>";
         echo "<td style='padding:0 60px'>".$row['docType']."</td>";
@@ -69,5 +83,5 @@ function listPending () {
         echo "<td>PDF file here</td>";
         echo "</tr>";
 
-    }
+    }*/
 }
