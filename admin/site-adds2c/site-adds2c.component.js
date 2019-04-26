1b13c8f58ee0bd424771a8c09d43871f8a5f3a3d
ryanraitz <46646466+ryanraitz@users.noreply.github.com>
2019-04-26 07:32:42 -0400

    new s2c files

---
+++ admin/site-adds2c/site-adds2c.component.js
@@ -0,0 +1,47 @@
+'use strict';
+angular.module('admin.addS2C').component('addS2C',{
+    //the url is relative to the index.html
+    templateUrl:'admin/site-adds2c/site-adds2c.view.html',
+    controller:
+    ['$scope','$http',
+        function SiteAddS2CController ($scope,$http) {
+
+
+            $scope.serviceSite;
+            $scope.siteDescript;
+            $scope.reportCode;
+            $scope.siteStatus;
+            $scope.siteID;
+
+            $scope.fkSiteID = sessionStorage.getItem('SiteID');
+
+
+            $scope.createS2C = function () {
+                var sendData =
+                    {
+                        serviceSite: $scope.serviceSite,
+                        siteDescript:$scope.siteDescript,
+                        reportCode:$scope.reportCode,
+                        siteStatus:$scope.siteStatus,
+                        siteID:$scope.siteID
+                    };
+
+                $http({
+                    method: 'POST',
+                    url: "./php/admin_addS2C.php",
+                    data: Object.toparams(sendData),
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                }).then(
+                    function (response) {
+                        if (response.data) {
+                            //$scope.msg="data updated";
+                            alert("data updated");
+                        }
+                        //location.reload(true);
+                    }, function (result) {
+                        alert("Failed");
+                    });
+            };
+        }
+    ]
+    });
