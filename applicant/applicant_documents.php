21c9773a198e95d55957f06a8f60b4d3244e1aaf
JackEveleigh <47282333+JackEveleigh@users.noreply.github.com>
2019-05-01 17:31:18 -0400

    Update

--- applicant/applicant_documents.php
+++ applicant/applicant_documents.php
@@ -3,13 +3,43 @@
 require_once './applicant_view.php';
 require_once '../php/dbcontroller.php';
 
+//pass appID from other page into each 3 functions
+//make sure file path can open document
+//fix which tables each function is pulling from
+
+function displayName() {
+    $appID = 12;
+
+    $connection = new DBController();
+    if (!$connection) die("Unable to connect to the database!");
+
+    $sql = "SELECT tblApplicants.lName, tblApplicants.fName FROM tblApplicants WHERE tblApplicants.applicantID = $appID";
+
+    $result = $connection->connectDB();
+    if (!$result)
+        die("Unable to perform query!");
+
+    $query = mysqli_query($result, $sql);
+
+    if (!$query) {
+        printf("Error: %s\n", mysqli_error($result));
+        exit();
+    }
+
+    while ($row = mysqli_fetch_array($query)) {
+
+        echo "Currently looking at documents for Applicant: " . $row['fName'] . " " . $row['lName'];
+
+    }
+
+}
 
 function listCompleted() {
     $appID = 12;
     $connection = new DBController();
     if (!$connection) die("Unable to connect to the database!");
 
-    $sql = "SELECT tblappdocs.docType, tblappdocs.dateUploaded FROM tblappdocs INNER JOIN tblCandidatePool ON tblCandidatePool.applicantID = tblappdocs.applicantID WHERE tblCandidatePool.applicantID = $appID";
+    $sql = "SELECT tblappdocs.docType, tblappdocs.dateUploaded, tblappdocs.filePath FROM tblappdocs INNER JOIN tblApplicants ON tblApplicants.applicantID = tblappdocs.applicantID WHERE tblApplicants.applicantID = $appID";
 
     $result = $connection->connectDB();
     if (!$result)
@@ -24,10 +54,10 @@ function listCompleted() {
 
 
     while ($row = mysqli_fetch_array($query)) {
-        //if(){}
+        $file = $row['filePath'];
         echo "<tr>";
         echo "<td style='padding:0 60px'>".$row['docType']."</td>";
-        echo "<td></td>";
+        echo "<td><button type='button' style='width:100px;' class='btn btn-success' onclick=\"location.href='$file'\">View</button></td>";
         echo "<td style='padding:0 60px'>".$row['dateUploaded']."</td>";
         echo "</tr>";
     }
@@ -35,10 +65,11 @@ function listCompleted() {
 
 function listPending () {
     $appID = 12;
+
     $connection = new DBController();
     if (!$connection) die("Unable to connect to the database!");
 
-    $sql = "SELECT tblappdocs.docType, tblappdocs.documentID, tblappdocs.dateUploaded FROM tblappdocs INNER JOIN tblCandidatePool ON tblCandidatePool.applicantID = tblappdocs.applicantID WHERE tblCandidatePool.applicantID = $appID";
+    $sql = "SELECT tblAppDocs.docType, tblAppDocs.documentID, tblAppDocs.dateUploaded FROM tblappdocs INNER JOIN tblApplicants ON tblApplicants.applicantID = tblappdocs.applicantID WHERE tblApplicants.applicantID = $appID";
 
     $result = $connection->connectDB();
     if (!$result)
@@ -55,6 +86,7 @@ function listPending () {
         "Candidate Application Document Form"=>'5', "Medical History Form"=>'6', "Copy of Birth Certificate"=>'7', "Legal History Form"=>'8', "Mentor Application"=>'9', "Copy of Social Security Card"=>'10',
         "Government Issued ID"=>'11', "Mental Health Information Form"=>'12');
 
+
     while ($row = mysqli_fetch_array($query)) {
         foreach($arr as $x => $x_value){
             foreach($row as $key => $value){
@@ -67,7 +99,8 @@ function listPending () {
             }
         }
     }
-    foreach($arr as $s => $s_value){
+
+        foreach($arr as $s => $s_value){
         echo "<tr>";
         echo "<td style='padding:0 60px; color:red;' >".$s."</td>";
         echo "<td><button type='button' style='width:100px;' class='btn btn-success'>Upload File</button></td>";
