284cbf04a61900a9f6417bb27ab0026b17666a78
Kali Kimball <kali.kimball@bobcats.gcsu.edu>
2019-05-03 14:35:53 -0400

    Update postres_getReporterLookup.php

--- php/postres_getReporterLookup.php
+++ php/postres_getReporterLookup.php
@@ -10,7 +10,7 @@ require_once 'dbcontroller.php';
 $connection = new DBController();
 
 $fields = "p.PersonFN,p.PersonLN,p.PersonID, q.PersonType";
-$sql = "SELECT DISTINCT ". $fields ." FROM tblpeople p, tblpersontypes t, tlkppersontype q WHERE p.PersonID = t.fkPersonID AND t.fkPersonTypeID = q.PersonTypeID AND t.fkPersonTypeID BETWEEN '0' AND '5' ORDER BY PersonLN ASC";
+$sql = "SELECT DISTINCT ". $fields ." FROM tblPeople p, tblPersonTypes t, tlkpPersonType q WHERE p.PersonID = t.fkPersonID AND t.fkPersonTypeID = q.PersonTypeID AND t.fkPersonTypeID BETWEEN '0' AND '5' ORDER BY PersonLN ASC";
 $result = $connection->runSelectQueryArrayNotEncoded($sql);
 echo '{"data":' . (json_encode($result)) . "} ";
 ?>
