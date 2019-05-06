c080fd6fef5bd7234ec07763320f810f1e7d91b4
ryanraitz <46646466+ryanraitz@users.noreply.github.com>
2019-05-05 20:37:43 -0400

    Added Life Skills

--- minerva-develop/core-components/core-components.module.js
+++ minerva-develop/core-components/core-components.module.js
@@ -8,6 +8,7 @@ angular.module('core-components', [
     'core-components.lead-follow',
     'core-components.physical-fitness',
     'core-components.s2c',
-    'core-components.job-skills'
+    'core-components.job-skills',
+    'core-components.life-skills'
 
 ]);
